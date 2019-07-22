4e4bb5a3cf290ab86ab2342960b61e2f8f1034c9
Artem Koshevoi <turkey2890755@gmail.com>
2019-07-22 19:00:07 +0300

    refactor(src/redux/todo/epics) change async/fetch requests to ajax

--- src/redux/todo/epics/epics.ts
+++ src/redux/todo/epics/epics.ts
@@ -16,110 +16,112 @@ import {
 import {catchError, map, mergeMap} from "rxjs/operators";
 import {from, of} from "rxjs";
 import {CustomAction} from "../reducers/reducer";
+import {ajax} from "rxjs/ajax";
+import {ajaxDelete, ajaxPatch, ajaxPost} from "rxjs/internal-compatibility";
 
 export const getTodoEpic = (action$: any) =>
   action$.pipe(
     ofType(GET_TODOS),
-    mergeMap(async () => {
-      const url = `http://localhost:3001/todos`;
-      const payload = await fetch(url).then(res => res.json());
-      return { type: SET_TODOS, payload };
-    }),
-    catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
+    mergeMap(() => ajax.getJSON('http://localhost:3001/todos').pipe(
+      map(response => ({ type: SET_TODOS, payload: response })),
+      catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message }))
+    ))
   );
 
 export const addTodoEpic = (action$: any) =>
   action$.pipe(
     ofType(PUT_TODO),
-    mergeMap(async (action: CustomAction) => {
-      const url = `http://localhost:3001/todos`;
-      const data = JSON.stringify({
+    mergeMap((action: CustomAction) => ajaxPost(
+      `http://localhost:3001/todos`,
+        JSON.stringify({
         id: Math.random(),
         name: action.payload.name,
         checked: false
-      });
-      const response = await fetch(
-        url,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: data,
-        },
-      )
-        .then(res => res.json());
-      return { type: ADD_TODO, payload: response };
-    }),
-    catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
+      }),
+      {
+        'Content-Type': 'application/json'
+      }).pipe(
+        map(res => ({ type: ADD_TODO, payload: res.response })),
+        catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message }))
+      ))
   );
 
 export const deleteTodoEpic = (action$: any) =>
   action$.pipe(
     ofType(DELETE_REQUEST_TODO),
-    mergeMap(async (action: CustomAction) => {
-      const url = `http://localhost:3001/todos/`;
-      await fetch(url + action.payload.id, {method: 'DELETE'}).then(res => res.json());
-      return { type: DELETE_TODO, payload: action.payload.id };
-    }),
-    catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
-  );
+    mergeMap( (action: CustomAction) => ajaxDelete(
+      `http://localhost:3001/todos/` + action.payload.id
+    ).pipe(
+      map(() => ({ type: DELETE_TODO, payload: action.payload.id })),
+      catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message }))
+    )));
 
 export const checkedTodoEpic = (action$: any) =>
   action$.pipe(
     ofType(CHECKED_REQUEST_TODO),
-    mergeMap(async (action: CustomAction) => {
-      const url = `http://localhost:3001/todos/`;
-      const data = {
+    mergeMap((action: CustomAction) => ajaxPatch(
+      `http://localhost:3001/todos/` + action.payload.id,
+      JSON.stringify({
         "checked": !action.payload.checked
-      };
-      await fetch(
-        url + action.payload.id,
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data),
-        }
-      )
-        .then(res => res.json());
-      return { type: CHECKED_TODO, payload: action.payload };
-    }),
-    catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
-  );
+      }),
+      {
+        'Content-Type': 'application/json'
+      }).pipe(
+        map(() => ({ type: CHECKED_TODO, payload: action.payload })),
+        catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message }))
+    )));
 
 export const changeCheckedTodoEpic = (action$: any) =>
   action$.pipe(
     ofType(CHANGE_CHECKED_REQUEST_TODO),
     mergeMap((action: CustomAction) => {
-      const url = `http://localhost:3001/todos/`;
-      const data = {
-        "checked": !action.payload.checkedStatus
-      };
-      return from(action.payload.checkedArr).pipe(
-        mergeMap(async (id: any) => {
-          await fetch(
-            url + id,
+        return from (action.payload.checkedArr).pipe(
+          mergeMap((id: any) => ajaxPatch(
+            `http://localhost:3001/todos/` + id,
+            JSON.stringify({
+              "checked": !action.payload.checkedStatus
+            }),
             {
-              method: 'PATCH',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data),
-            }
+              'Content-Type': 'application/json'
+            })
           )
-            .then(res => res.json());
-          return action.payload;
-        }),
-      );
-    }),
-    map((payload) => {
-      return { type: CHANGE_CHECKED_TODO, payload };
-    }),
-    catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
-  );
+        )}
+    ),
+    map((payload) => ({ type: CHANGE_CHECKED_TODO, payload }))
+    );
 
+
+
+// export const changeCheckedTodoEpic = (action$: any) =>
+//   action$.pipe(
+//     ofType(CHANGE_CHECKED_REQUEST_TODO),
+//     mergeMap((action: CustomAction) => {
+//       const url = `http://localhost:3001/todos/`;
+//       const data = {
+//         "checked": !action.payload.checkedStatus
+//       };
+//       return from(action.payload.checkedArr).pipe(
+//         mergeMap(async (id: any) => {
+//           await fetch(
+//             url + id,
+//             {
+//               method: 'PATCH',
+//               headers: {
+//                 'Content-Type': 'application/json'
+//               },
+//               body: JSON.stringify(data),
+//             }
+//           )
+//             .then(res => res.json());
+//           return action.payload;
+//         }),
+//       );
+//     }),
+//     map((payload) => {
+//       return { type: CHANGE_CHECKED_TODO, payload };
+//     }),
+//     catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
+//   );
 
 export const deleteCheckedTodoEpic = (action$: any) =>
   action$.pipe(
@@ -139,26 +141,36 @@ export const deleteCheckedTodoEpic = (action$: any) =>
     catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
   );
 
+// export const deleteCheckedTodoEpic = (action$: any) =>
+//   action$.pipe(
+//     ofType(DELETE_CHECKED_REQUEST_TODO),
+//     mergeMap((action: CustomAction) => {
+//       const url = `http://localhost:3001/todos/`;
+//       return from(action.payload.checkedArr).pipe(
+//         mergeMap(async (id: any) => {
+//           await fetch(url + id, {method: 'DELETE'});
+//           return action.payload;
+//         }),
+//       );
+//     }),
+//     map((payload) => {
+//       return { type: DELETE_CHECKED_TODO, payload };
+//     }),
+//     catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
+//   );
+
 export const updateTodoEpic = (action$: any) =>
   action$.pipe(
     ofType(UPDATE_REQUEST_TODO),
-    mergeMap(async (action: CustomAction) => {
-      const url = `http://localhost:3001/todos/`;
-      const data = {
-        "name": action.payload.value
-      };
-      await fetch(
-        url + action.payload.id,
-        {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data),
-        }
+    mergeMap((action: CustomAction) => ajaxPatch(
+      `http://localhost:3001/todos/` + action.payload.id,
+      JSON.stringify({
+        "name": action.payload.value,
+      }),
+      {
+        'Content-Type': 'application/json'
+      }).pipe(
+        map(() => ({ type: UPDATE_TODO, payload: action.payload })),
+        catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message }))
       )
-        .then(res => res.json());
-      return { type: UPDATE_TODO, payload: action.payload };
-    }),
-    catchError(err => of({ type: GET_TODOS_ERROR, payload: err.message })),
-  );
+    ));
